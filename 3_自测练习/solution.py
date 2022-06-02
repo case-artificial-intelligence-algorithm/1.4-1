e7aec5ceb7691f838833251c8c1cdb141b31e631
sn-911 <512428493@qq.com>
2022-06-02 23:45:52 +0800

    modified files

--- 3_自测练习/solution.py
+++ 3_自测练习/solution.py
@@ -1,4 +1,4 @@
-# 利用哈夫曼编码算法构造表示哈夫曼树
+# 利用哈夫曼编码算法构造表示最优前缀码的二叉树T
 class Node(object):
     # 定义二叉树T中节点的数据结构
     def __init__(self, name=None, value=None):
@@ -19,7 +19,18 @@ class HuffmanTree(object):
         self.root = self.queue[0]
         # queue长度不为1时，对队列中的元素执行哈夫曼算法
         while len(self.queue) != 1:
-            raise NotImplementedError('请用哈夫曼算法构造哈夫曼树')
+            # 将queue中的元素按节点的权值从大到小的顺序排序
+            self.queue.sort(key=lambda node: node.value, reverse=True)
+            # 从队列中选取权值最小的两个节点生成一个新节点t
+            t = Node(value=(self.queue[-1].value + self.queue[-2].value))
+            # t的左孩子是此时queue中权值最小的节点，且将该权值最小的节点从队列中删除
+            t.left = self.queue.pop(-1)
+            # t的右孩子是此时queue中权值最小的节点，且将该权值最小的节点从队列中删除
+            t.right = self.queue.pop(-1)
+            # 将新节点t加入到queue中
+            self.queue.append(t)
+            # root表示哈夫曼树的根节点b
+            self.root = self.queue[0]
 
     # 编码输出
     def encode(self, tree, length):
@@ -45,5 +56,12 @@ class HuffmanTree(object):
     def get_code(self):
         self.encode(self.root, 0)
 
+
 if __name__ == '__main__':
-    pass
+    C = [('a', 18), ('b', 9), ('c', 24), ('d', 4), ('e', 30), ('f', 15)]
+    # 初始化哈夫曼树
+    tree = HuffmanTree(C)
+    # 哈夫曼编码
+    tree.get_code()
+    # 获取每个元素的哈夫曼编码
+    print(tree.result)
--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
-from my_solution import solution
+from my_solution import HuffmanTree
 
 
 # 测试用例
 def test_solution():
-    assert solution(8) == 9  # 判断输出结果，预期increment(8)应该为9
+    C = [('a', 18), ('b', 9), ('c', 24), ('d', 4), ('e', 30), ('f', 15)]
+    # 初始化哈夫曼树
+    tree = HuffmanTree(C)
+    # 哈夫曼编码
+    tree.get_code()
+    #获取每个元素的哈夫曼编码
+    result=tree.result
+    # 正确答案
+    correct_solution = ['a的编码为:00',
+                        'c的编码为:01',
+                        'd的编码为:1000',
+                        'b的编码为:1001',
+                        'f的编码为:101',
+                        'e的编码为:11']
+
+
+    assert correct_solution == result
